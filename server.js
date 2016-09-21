25f6db940621eb25649e5b3c49302b5239d5a66d
Matt Britt <matthewlbritt@gmail.com>
2016-09-21 02:27:52 +0000

    Added /whoami/ microservice; update landing page

--- server.js
+++ server.js
@@ -13,21 +13,36 @@ app.get('/', function(req, res){
 //example json for timestamp:
    var timestamp_ob = {'unix': 1450137600,
              'natural': "December 15, 2015" };
+
+//example json for whoami:
+    var whoami_ob = {"ipaddress":"24.155.221.251",
+                    "language":"en-US",
+                    "software":"Windows NT 10.0; Win64; x64"};
+
+
              
-   res.render('index',{timestamp:JSON.stringify(timestamp_ob)}); 
+   res.render('index',{timestamp:JSON.stringify(timestamp_ob),whoami:JSON.stringify(whoami_ob)}); 
     
 });
 
 app.get('/timestamp/:dateStr', function(req, res){
 //timestamp service (reachable at'/timestamp')
 
-    var ts = require('./timestamp');
+    var ts = require('./timestamp');                            //use timestamp.js module for logic
+    res.jsonp(ts(req.params.dateStr));
     
-    res.end(ts(req.params.dateStr));
- 
+    res.end();
 });
 
 
+app.get('/whoami/', function(req, res){
+//request header parser will live at /whoami/
+
+    var whoami = require('./whoami');                           //use whoami.js module for logic
+    res.jsonp(whoami(req)); 
+    
+    res.end();
+});
 
 
 app.listen(port);
